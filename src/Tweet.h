4ee7796f0d5b76ef82c6ba40c5bbc6f2e1998512
Giancarlos Dominguez <Giancarlosd@smu.edu>
2023-03-02 23:53:48 -0600

    Added Tweet class

---
+++ src/Tweet.h
@@ -0,0 +1,13 @@
+#ifndef SENTIMENT_ANALYZER_C__TWEET_H
+#define SENTIMENT_ANALYZER_C__TWEET_H
+
+#include <iostream>
+
+class Tweet {
+private:
+public:
+    explicit Tweet();
+};
+
+
+#endif //SENTIMENT_ANALYZER_C__TWEET_H
