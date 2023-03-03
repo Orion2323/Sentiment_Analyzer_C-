0f980306d4b0208659f9b87ddbeacbae9431f62f
Giancarlos Dominguez <Giancarlosd@smu.edu>
2023-03-02 23:53:58 -0600

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
 
+#include "Sentiment_Analyzer.h"
+
 using namespace std;
 
 int main() {
     cout << "Hello World!" << endl;
+
+    Sentiment_Analyzer s = Sentiment_Analyzer();
 
     return 0;
 }
