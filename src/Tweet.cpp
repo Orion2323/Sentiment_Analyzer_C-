e2e2d8ca1a7c9282f96803f24626d676ad7d4e5b
Giancarlos Dominguez <Giancarlosd@smu.edu>
2023-03-04 13:48:49 -0600

    Added methods for object funtionality

--- src/Tweet.cpp
+++ src/Tweet.cpp
@@ -1,1 +1,39 @@
 #include "Tweet.h"
+
+Tweet::Tweet() {
+    this->senVal = 0;
+    this->ID = 0;
+
+    this->date;
+    this->user;
+    this->tweet;
+};
+
+Tweet::Tweet(const int& senVal, const long& ID, const char* date, const char* user, const char* tweet) {
+    this->senVal = senVal;
+    this->ID = ID;
+
+    this->date = DSString(date);
+    this->user = DSString(user);
+    this->tweet = DSString(tweet);
+}
+
+DSString Tweet::getTweet() const {
+    return this->tweet;
+}
+
+DSString Tweet::getUser() const {
+    return this->user;
+}
+
+DSString Tweet::getDate() const {
+    return this->date;
+}
+
+int Tweet::getSen() const {
+    return this->senVal;
+}
+
+long Tweet::getID() const {
+    return this->ID;
+}
--- src/Tweet.h
+++ src/Tweet.h
@@ -3,11 +3,24 @@
 
 #include <iostream>
 
+#include "DSString.h"
+
 class Tweet {
 private:
+    int senVal;
+    long ID;
+    DSString date;
+    DSString user;
+    DSString tweet;
 public:
     explicit Tweet();
-};
+    explicit Tweet(const int& senVal, const long& ID, const char* date, const char* user, const char* tweet);
 
+    DSString getTweet() const;
+    DSString getUser() const;
+    DSString getDate() const;
+    int getSen() const;
+    long getID() const;
+};
 
 #endif //SENTIMENT_ANALYZER_C__TWEET_H
